54aab30bfc9c2447bcb37faf40d52a987428e7f4
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2024-09-10 19:15:02 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,17 @@
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
-
 def handler(req, res):
-    s = req.url
-    dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-    res.status_code = 200
-    res.headers['Content-Type'] = 'text/plain'
+    # Get the query parameters from the URL
+    query_params = req.query
 
-    if "name" in dic:
-        message = "Hello, " + dic["name"] + "!"
+    # Create a response message based on query parameters
+    if "name" in query_params:
+        message = f"Hello, {query_params['name']}!"
     else:
         message = "Hello, stranger!"
 
+    # Set the response status and headers
+    res.status_code = 200
+    res.headers['Content-Type'] = 'text/plain'
+    
+    # Send the response
     res.body = message
     return res
