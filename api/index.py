556fa465be6a07054d83d717796090240478c511
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2024-09-10 19:11:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,66 +1,16 @@
-from fasthtml.common import *
-import redis
-from tinyredis import TinyRedis
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
 
-xtra_css = Style(':root { --pico-font-size: 100%; }')
-app = FastHTML(secret_key=os.getenv('SESSKEY', 's3kret'),
-               hdrs=[picolink, xtra_css, SortableJS('.sortable')])
-rt = app.route
+def handler(req, res):
+    s = req.url
+    dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+    res.status_code = 200
+    res.headers['Content-Type'] = 'text/plain'
 
-@dataclass
-class Todo: id:str=None; title:str=''; done:bool=False; priority:int=0
-todos = TinyRedis(redis.from_url(os.environ['VERCEL_KV_URL']), Todo)
-def tid(id): return f'todo-{id}'
+    if "name" in dic:
+        message = "Hello, " + dic["name"] + "!"
+    else:
+        message = "Hello, stranger!"
 
-@patch
-def __ft__(self:Todo):
-    show = AX(self.title, f'/todos/{self.id}', 'current-todo')
-    edit = AX('edit',     f'/edit/{self.id}' , 'current-todo')
-    dt = ' ✅' if self.done else ''
-    cts = (dt, show, ' | ', edit, Hidden(id="id", value=self.id), Hidden(id="priority", value="0"))
-    return Li(*cts, id=f'todo-{self.id}')
-
-def mk_input(**kw): return Input(id="new-title", name="title", placeholder="New Todo", **kw)
-
-@rt("/")
-async def get():
-    add = Form(Group(mk_input(), Button("Add")),
-               hx_post="/", target_id='todo-list', hx_swap="beforeend")
-    items = sorted(todos(), key=lambda o: o.priority)
-    frm = Form(*items, id='todo-list', cls='sortable', hx_post="/reorder", hx_trigger="end")
-    return Titled('Todo list', Card(Ul(frm), header=add, footer=Div(id='current-todo')))
-
-@rt("/reorder")
-def post(id:list[str]):
-    items = todos()
-    pos = {u:i for i,u in enumerate(id)}
-    for o in items: o.priority=pos[o.id]
-    todos.insert_all(items)
-    return tuple(sorted(items, key=lambda o: o.priority))
-
-@rt("/todos/{id}")
-async def delete(id:str):
-    todos.delete(id)
-    return clear('current-todo')
-
-@rt("/")
-async def post(todo:Todo): return todos.insert(todo), mk_input(hx_swap_oob='true')
-
-@rt("/edit/{id}")
-async def get(id:str):
-    res = Form(Group(Input(id="title"), Button("Save")),
-        Hidden(id="id"), CheckboxX(id="done", label='Done'),
-        hx_put="/", target_id=tid(id), id="edit")
-    return fill_form(res, todos[id])
-
-@rt("/")
-async def put(todo: Todo): return todos.update(todo), clear('current-todo')
-
-@rt("/todos/{id}")
-async def get(id:str):
-    todo = todos[id]
-    btn = Button('delete', hx_delete=f'/todos/{todo.id}',
-                 target_id=tid(todo.id), hx_swap="outerHTML")
-    return Div(Div(todo.title), btn)
-
-serve()
+    res.body = message
+    return res
